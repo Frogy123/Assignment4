855a738c3c711911114b11e3006c0cd5cafdbddd
David <davidpaster2018@gmail.com>
2025-02-07 22:54:50 +0200

    implemented action

--- action.py
+++ action.py
@@ -7,7 +7,24 @@ def main(args : list[str]):
     with open(inputfilename) as inputfile:
         for line in inputfile:
             splittedline : list[str] = line.strip().split(", ")
-            #TODO: apply the action (and insert to the table) if possible
+            product_id : int = int(splittedline[0])
+            quantity : int = int(splittedline[1])
+            activator_id : int = int(splittedline[2])
+            date : str = splittedline[3]
+            product = repo.products.find(id = product_id).fetchone()[0]
+            #if sale - check if there is enough enough quantity
+            if(quantity < 0 and product.quantity < abs(quantity)):
+                #not enough quantity - do nothing
+                continue
+            else:
+                repo.activities.insert(Activitie(product_id, quantity, activator_id, date))
+                repo.products.update({"quantity" : (product.quantity + quantity)}, id = product_id)
+
+            
+
+            
+
+
 
 if __name__ == '__main__':
     main(sys.argv)
--- dbtools.py
+++ dbtools.py
@@ -66,3 +66,19 @@ class Dao(object):
                .format(self._table_name,' AND '.join([col + '=?' for col in column_names]))
  
         self._conn.cursor().execute(stmt, params)
+
+#UPDATE: as seen in PS 13
+    def update(self, set_values, **cond):
+        set_column_names = set_values.keys()
+        set_params = set_values.values()
+
+        cond_column_names = cond.keys()
+        cond_params = cond.values()
+
+        params = set_params + cond_params
+
+        stmt = 'UPDATE {} SET ({}) WHERE ({})'.format(self._table_name,
+                                                      ', '.join([set + '=?' for set in set_column_names]),
+                                                      ' AND '.join([cond + '=?' for cond in cond_column_names]))
+
+        self._conn.execute(stmt, params)
